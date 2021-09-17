f93f11a5271dc91a0abc8e78eef35a8c1cc6e636
Fritz Hund <edifice444@gmail.com>
2021-09-17 15:30:52 +0200

    Add 1 library for BV and functionality for shopitems

---
+++ bettervoiding_4444444444/libs/tableEx.lua
@@ -0,0 +1,55 @@
+------------------------------------------
+-- Local/Global variables and constants
+------------------------------------------
+TableEx = {}
+
+
+----------------------------------------------------
+-- Removes all entrys with nil values in keyTable
+----------------------------------------------------
+--       !!! Doesn't obtain Positions !!!         --
+function TableEx.updateTable(keyTable)
+    local updatedTable = {}
+    for k,v in pairs(keyTable) do
+        if not (v == nil) then
+            updatedTable[k] = v
+        end
+    end
+    return updatedTable
+end
+
+------------------------------------------------
+-- Returns key for lowest value in a keyTable
+------------------------------------------------
+function TableEx.getKeyOfLowestValue(keyTable)
+    local key = nil
+    local value = nil
+
+    for k,v in pairs(keyTable) do
+        if (value == nil or v < value) then
+            value = v
+            key = k
+        end
+    end
+
+    return key
+end
+
+------------------------------------------------
+-- Returns key for highest value in a keyTable
+------------------------------------------------
+function TableEx.getKeyOfHighestValue(keyTable)
+    local key = nil
+    local value = nil
+
+    for k,v in pairs(keyTable) do
+        if (value == nil or v > value) then
+            value = v
+            key = k
+        end
+    end
+
+    return key
+end
+
+return TableEx
--- bettervoiding_4444444444/main.lua
+++ bettervoiding_4444444444/main.lua
@@ -1,26 +1,32 @@
+-----------------------------
+-- Requiered Modules
+-----------------------------
+require("libs.entityMath")
+require("libs.tableEx")
+
 local BetterVoiding = RegisterMod("Better Voiding", 1)
 
-local debugText = 0 --Test
 
--------------------------------------
--- Local variables and constants
--------------------------------------
+----------------------------------------------------
+-- Test
+local debugText = 0
+
+function BetterVoiding:drawDebugText()
+    --bloodyPayNearestItem(Isaac.GetPlayer())
+    Isaac.RenderText(debugText, 100, 100, 255, 0, 0, 255)
+end
+
+BetterVoiding:AddCallback(ModCallbacks.MC_POST_RENDER, BetterVoiding.drawDebugText)
+----------------------------------------------------
+
+
+
+-----------------------------------------
+-- Local/Global variables and constants
+-----------------------------------------
 local game = Game()
 local collDist = {}
 local usedColls = {}
-
-----------------------------------------------------
--- Removes all entrys with nil values in keyTable
-----------------------------------------------------
-local function updateTable(keyTable)
-    local updatedTable = {}
-    for k,v in pairs(keyTable) do
-        if not (v == nil) then
-            updatedTable[k] = v
-        end
-    end
-    return updatedTable
-end
 
 
 -------------------------------------
@@ -32,135 +38,141 @@ local function vecDistance2D(pos1, pos2)
     return math.sqrt((pos1.X-pos2.X)^2 + (pos1.Y-pos2.Y)^2)
 end
 
--- Returns key for lowest value in a keyTable
-local function getKeyOfLowestValue(keyTable)
-    local key = nil
-    local value = nil
-
-    updateTable(keyTable)
-
-    for k,v in pairs(keyTable) do
-        if (value == nil or v < value) then
-            value = v
-            key = k
-        end
-    end
-    if (value ~= nil) then
-       return key
-    else
-        return nil
-    end
-end
-
 -------------------------------------------------------------------
--- Determins all collectibles and its distance to the sourceEntity
+-- Determins all collectibles and its distance to the playerEntity
 -------------------------------------------------------------------
-local function calculateCollDist(sourceEntity)
+local function calculateCollDist(playerEntity)
     collDist = {}
     local allEntities = Isaac.GetRoomEntities()
     for _,collEntity in pairs(allEntities) do    -- Filter room for collectibles
         if (collEntity.Type == EntityType.ENTITY_PICKUP and collEntity.Variant == PickupVariant.PICKUP_COLLECTIBLE) then
-            collDist[collEntity] = vecDistance2D(sourceEntity.Position, collEntity.Position)
+            collDist[collEntity] = vecDistance2D(playerEntity.Position, collEntity.Position)
         end
     end
 end
 
----------------------------------------
--- 
----------------------------------------
+--------------------------------------------------------
+-- Removes entity from the game and the collDist table
+--------------------------------------------------------
 local function despawnEntity(entity)
     collDist[entity] = nil
+    collDist = TableEx.updateTable(collDist)
     entity:Remove()
 end
 
+--------------------------------------------------------
+-- Pay nearest item in a heart deal
+-------------------------------------------------------
+local function payNearestItem(sourceEntity)
+
+    local itemEntity = BetterVoiding:getNearestItem(sourceEntity)
+
+    if itemEntity == nil then
+        return nil
+    end
+
+    local itemPickup = itemEntity:ToPickup()
+
+    if (itemEntity:ToPickup().Price ~= 0) then    --Item has price
+
+        -- Kill entity if it's not the first player
+        if (GetPtrHash(sourceEntity) ~= GetPtrHash(Isaac.GetPlayer())) then
+            sourceEntity.Kill()
+        else
+            local playerEntity = sourceEntity:ToPlayer()
+            local itemPrice = itemPickup.Price
+
+            -- Player pay price for the item if he can
+            if itemPrice == PickupPrice.PRICE_ONE_HEART then
+                if playerEntity:GetMaxHearts() < 2 then
+                    return nil
+                end
+                playerEntity:AddMaxHearts(-2)
+
+            elseif itemPrice == PickupPrice.PRICE_TWO_HEARTS then
+                local maxHearts = playerEntity:GetMaxHearts()
+                if maxHearts < 2 then
+                    return nil
+                elseif maxHearts >= 4 then
+                    maxHearts = 4
+                end
+                playerEntity:AddMaxHearts(-maxHearts)
+
+            elseif itemPrice == PickupPrice.PRICE_THREE_SOULHEARTS then
+                local maxHeartsSoul = playerEntity:GetSoulHearts()
+                if maxHeartsSoul < 1 then
+                    return nil
+                elseif maxHeartsSoul >= 6 then
+                    maxHeartsSoul = 6
+                end
+                playerEntity:AddSoulHearts(-maxHeartsSoul)
+
+            elseif itemPrice == PickupPrice.PRICE_ONE_HEART_AND_TWO_SOULHEARTS then
+                local maxHearts = playerEntity:GetMaxHearts()
+                local maxHeartsSoul = playerEntity:GetSoulHearts()
+                if maxHearts < 2 then
+                    return nil
+                elseif maxHeartsSoul > 4 then
+                    maxHeartsSoul = 4
+                end
+                playerEntity:AddMaxHearts(-maxHearts)
+                playerEntity:AddSoulHearts(-maxHeartsSoul)
+
+            elseif itemPrice == PickupPrice.PRICE_SPIKES then
+                return nil
+
+            elseif itemPrice == PickupPrice.PRICE_SOUL then
+                -- ?
+                return nil
+
+            elseif itemPrice > 0 then
+                local playersCoins = playerEntity:GetNumCoins()
+                if (itemPrice > playersCoins) then
+                    return nil
+                else
+                    playerEntity:AddCoins(-itemPrice)
+                end
+
+            else
+                return nil
+            end
+        end
+        -- Exchange item
+        despawnEntity(itemEntity) -- Despawn item
+        Isaac.Spawn(itemPickup.Type, itemPickup.Variant, itemPickup.SubType, itemEntity.Position, Vector(0,0), nil) -- Spawn item without price
+
+        -- Devildeals only
+        if game:GetRoom():GetType() == RoomType.ROOM_DEVIL then
+            game:AddDevilRoomDeal()
+        end
+
+        return itemPickup
+    end
+
+    return nil -- If entity is nil or item with no price
+end
+
+--------------------------------------------------------
+-- Remove all items in the room exept the nearest Item
+--------------------------------------------------------
+local function removeOtherItems()
+    payNearestItem(Isaac.GetPlayer())
+    return nil
+end
 
 ------------------------------------------------------
--- Returns nearest collectible to the sourceEntity
+-- Returns nearest collectible to the playerEntity
 ------------------------------------------------------
-function BetterVoiding:getNearestItem(sourceEntity)
+function BetterVoiding:getNearestItem(playerEntity)
     local nearestItem = nil
 
-    calculateCollDist(sourceEntity)
-    nearestItem = getKeyOfLowestValue(collDist)
+    calculateCollDist(playerEntity)
+    nearestItem = TableEx.getKeyOfLowestValue(collDist)
     if (nearestItem ~= nil) then
         table.insert(usedColls, 1, nearestItem)
     end
     return nearestItem
 end
 
---------------------------------------------------------
--- Pay nearest item in a heart deal
--------------------------------------------------------
-local function bloodyPayNearestItem(entity)
-    local itemEntity = BetterVoiding:getNearestItem(entity)
-    if (itemEntity ~= nil and itemEntity:ToPickup().Price ~= PickupPrice.PRICE_FREE and itemEntity:ToPickup().Price ~= 0) then
-        local itemPickup = itemEntity:ToPickup()
-
-        -- Kill entity if it's not the first player
-        if (GetPtrHash(entity) ~= GetPtrHash(Isaac.GetPlayer())) then
-            entity.Kill()
-            return nil
-        end
-
-        -- Pay hearts for the item
-        if itemPickup.Price == PickupPrice.PRICE_ONE_HEART then
-            if entity:GetMaxHearts() < 2 then
-                return nil
-            end
-            entity:AddMaxHearts(-2)
-        end
-        if itemPickup.Price == PickupPrice.PRICE_TWO_HEARTS then
-            if entity:GetMaxHearts() < 4 then
-                return nil
-            end
-            entity:AddMaxHearts(-4)
-        end
-        if itemPickup.Price == PickupPrice.PRICE_THREE_SOULHEARTS then
-            if entity:GetSoulHearts() < 6 then
-                return nil
-            end
-            entity:AddSoulHearts(-6)
-        end
-        if itemPickup.Price == PickupPrice.PRICE_ONE_HEART_AND_TWO_SOULHEARTS then
-            if entity:GetMaxHearts() < 2 or entity:GetSoulHearts() < 4 then
-                return nil
-            end
-            entity:AddMaxHearts(-2)
-            entity:AddSoulHearts(-4)
-        end
-        if itemPickup.Price == PickupPrice.PRICE_SPIKES then
-            return nil
-        end
-        if itemPickup.Price == PickupPrice.PRICE_SOUL then
-            -- ?
-            return nil
-        end
-        -- Exchange item
-        despawnEntity(itemEntity) -- Despawn item
-        Isaac.Spawn(itemPickup.Type, itemPickup.Variant, itemPickup.SubType, itemEntity.Position, Vector(0,0), nil) -- Spawn item without price
-        game:AddDevilRoomDeal() -- Mimics: Deal-Taken
-        return itemPickup
-    end 
-    
-    return nil -- If entity is nil or free
-end
-
-
---------------------------------------------------------
--- Remove all items in the room exept the nearest Item
---------------------------------------------------------
-local function removeOtherItems()
-    bloodyPayNearestItem(Isaac.GetPlayer())
-    return nil
-end
-
-
-function BetterVoiding:drawDebugText()
-    --bloodyPayNearestItem(Isaac.GetPlayer())
-    Isaac.RenderText(debugText, 100, 100, 255, 0, 0, 255)
-end
-
 BetterVoiding:AddCallback(ModCallbacks.MC_PRE_USE_ITEM, removeOtherItems, Isaac.GetItemIdByName("Void"))
-
-
-BetterVoiding:AddCallback(ModCallbacks.MC_POST_RENDER, BetterVoiding.drawDebugText)
+BetterVoiding:AddCallback(ModCallbacks.MC_PRE_USE_ITEM, removeOtherItems, Isaac.GetItemIdByName("Abyss"))

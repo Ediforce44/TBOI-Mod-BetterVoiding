05cb24509968bf9d5f09aab320c1f0bf676eaea6
Fritz Hund <edifice444@gmail.com>
2021-09-22 04:51:07 +0200

    BV: Complete overhaul of the collDist model

--- bettervoiding_4444444444/main.lua
+++ bettervoiding_4444444444/main.lua
@@ -1,86 +1,81 @@
 -----------------------------
 -- Requiered Modules
 -----------------------------
+
 require("libs.tableEx")
-
-local BetterVoiding = RegisterMod("Better Voiding", 1)
-
-
-----------------------------------------------------
--- Test
-local debugText = ""
-
-function BetterVoiding:drawDebugText()
-    Isaac.RenderText(debugText, 50, 50, 255, 0, 0, 255)
-end
-
-BetterVoiding:AddCallback(ModCallbacks.MC_POST_RENDER, BetterVoiding.drawDebugText)
-----------------------------------------------------
-
 
 
 ----------------------------------------------------------
 -- Global/Local variables and constants and Getter/Setter
 ----------------------------------------------------------
+
+local BetterVoiding = RegisterMod("Better Voiding", 1)
+
 local game = Game()
-local collDist = {}
-local tYourSoulID = TrinketType.TRINKET_YOUR_SOUL
 
 
--------------------------------------------------------------------
--- Determins all collectibles and its distance to the playerEntity
---      <<< !Needs to be called before everything else! >>>
--------------------------------------------------------------------
-local function calculateCollDist(playerEntity)
-    collDist = {}
+---------------------------------------------------------------------------------------------------------
+-- Determins all collectibles in the current room and their distance to the sourceEntity
+----- @Return: Table of (Keys: Collectibles, Values: Distance between the collectible and sourceEntity)
+---------------------------------------------------------------------------------------------------------
+local function calculateCollDist(sourceEntity)
+    local collDist = {}
     local allEntities = Isaac.GetRoomEntities()
 
     for _,collEntity in pairs(allEntities) do    -- Filter room for collectibles
         if (collEntity.Type == EntityType.ENTITY_PICKUP and collEntity.Variant == PickupVariant.PICKUP_COLLECTIBLE 
                 and collEntity.SubType ~= CollectibleType.COLLECTIBLE_NULL) then
-            collDist[collEntity:ToPickup()] = playerEntity.Position:Distance(collEntity.Position)
+            collDist[collEntity:ToPickup()] = sourceEntity.Position:Distance(collEntity.Position)
         end
     end
-end
-
---------------------------------------------------------
--- Removes the item from the game and the collDist table
---------------------------------------------------------
-local function despawnItem(itemPickup)
-    collDist[itemPickup] = nil
-    collDist = TableEx.updateTable(collDist)
-    itemPickup:Remove()
+    return collDist
 end
 
 -------------------------------------------------------------------------
 -- Removes all items with the same OptionsPickupIndex as the itemPickup
+----- @Retrun: itemPickup if it is payed
 -------------------------------------------------------------------------
 local function managePickupIndex(itemPickup)
     if itemPickup == nil then
-        return
+        return nil
     end
 
     local index = itemPickup.OptionsPickupIndex
     if index ~= 0 then
-        for item,_ in pairs(collDist) do
-            if (item ~= itemPickup and item.OptionsPickupIndex == index) then
+        for item,_ in pairs(calculateCollDist(Isaac.GetPlayer())) do
+            if (GetPtrHash(item) ~= GetPtrHash(itemPickup) and item.OptionsPickupIndex == index) then
                 Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 1, item.Position, Vector(0,0), item)
-                despawnItem(item)
+                item:Remove()
             end
         end
     end
+
+    --[[
+    if itemPickup:IsShopItem() then
+        return nil
+    else
+        return itemPickup
+    end--]]
+    return itemPickup:IsShopItem() and itemPickup or nil
 end
 
-----------------------------------------------------------------------------------------------
--- Removes all items with the same OptionsPickupIndex except the nearest items for each index
-----------------------------------------------------------------------------------------------
-local function manageAllPickupIndices()
+---------------------------------------------------------------------------------------------------------------
+-- Removes all items with the same OptionsPickupIndex except the nearest items to sourceEntity for each index
+----- @Return: Table of (Keys: Items, Values: Distance between the item and sourceEntity)
+---------------------------------------------------------------------------------------------------------------
+local function manageAllPickupIndices(sourceEntity)
     local indexTables = {}
     local index = 0
+    local remainingItems = {}
 
-    for item,dist in pairs(collDist) do
+    for item,dist in pairs(calculateCollDist(sourceEntity)) do
+        if item:IsShopItem() then --ignore shop items
+            goto continue
+        end
+
         index = item.OptionsPickupIndex
-        if (index == 0) then -- skip this iteration
+        if (index == 0) then      --items without OptionsPickupIndex
+            remainingItems[item] = dist
             goto continue
         end
 
@@ -93,9 +88,16 @@ local function manageAllPickupIndices()
     end
 
     for _,table in pairs(indexTables) do
-        local item = TableEx.getKeyOfLowestValue(table)
-        managePickupIndex(item)
+        local nearestItem = TableEx.getKeyOfLowestValue(table)
+        remainingItems[nearestItem] = table[nearestItem]
+        for item,_ in pairs(table) do
+            if not (item == nearestItem) then
+                Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 1, item.Position, Vector(0,0), item)
+                item:Remove()
+            end
+        end
     end
+    return remainingItems
 end
 
 ----------------------------------------------
@@ -103,103 +105,105 @@ end
 ----- @Return: Nearest item
 ----------------------------------------------
 function BetterVoiding:getNearestItem(sourceEntity)
-    local nearestItem = nil
-
-    calculateCollDist(sourceEntity)
-    nearestItem = TableEx.getKeyOfLowestValue(collDist)
-    return nearestItem
+    return TableEx.getKeyOfLowestValue(calculateCollDist(sourceEntity))
 end
 
--------------------------------------
--- Pay nearest item to sourceEntity
------ @Return: Nearest item
--------------------------------------
-function BetterVoiding:payNearestItem(sourceEntity)
+------------------------------------------
+-- Let sourceEntity pay for itemPickup
+----- @Return: Payed item
+------------------------------------------
+function BetterVoiding:payItem(itemPickup, sourceEntity)
+    local playerEntity = nil
+    local itemPrice = nil
 
-    local itemPickup = BetterVoiding:getNearestItem(sourceEntity)
-
-    if itemPickup == nil then
+    if itemPickup == nil or sourceEntity == nil then
         return nil
     end
 
-    if (itemPickup:IsShopItem()) then    --Item has price
-
-        -- Kill entity if it's not the first player
-        if (GetPtrHash(sourceEntity) ~= GetPtrHash(Isaac.GetPlayer())) then
-            sourceEntity.Kill()
-        else
-            local playerEntity = sourceEntity:ToPlayer()
-            local itemPrice = itemPickup.Price
-
-            -- Player pay price for the item if he can
-            if itemPrice == PickupPrice.PRICE_ONE_HEART then
-                if playerEntity:GetMaxHearts() < 2 then
-                    return nil
-                end
-                playerEntity:AddMaxHearts(-2)
-
-            elseif itemPrice == PickupPrice.PRICE_TWO_HEARTS then
-                local maxHearts = playerEntity:GetMaxHearts()
-                if maxHearts < 2 then
-                    return nil
-                elseif maxHearts >= 4 then
-                    maxHearts = 4
-                end
-                playerEntity:AddMaxHearts(-maxHearts)
-
-            elseif itemPrice == PickupPrice.PRICE_THREE_SOULHEARTS then
-                local maxHeartsSoul = playerEntity:GetSoulHearts()
-                if maxHeartsSoul < 1 then
-                    return nil
-                elseif maxHeartsSoul >= 6 then
-                    maxHeartsSoul = 6
-                end
-                playerEntity:AddSoulHearts(-maxHeartsSoul)
-
-            elseif itemPrice == PickupPrice.PRICE_ONE_HEART_AND_TWO_SOULHEARTS then
-                local maxHearts = playerEntity:GetMaxHearts()
-                local maxHeartsSoul = playerEntity:GetSoulHearts()
-                if maxHearts < 2 then
-                    return nil
-                elseif maxHeartsSoul > 4 then
-                    maxHeartsSoul = 4
-                end
-                playerEntity:AddMaxHearts(-maxHearts)
-                playerEntity:AddSoulHearts(-maxHeartsSoul)
-
-            elseif itemPrice == PickupPrice.PRICE_SPIKES then
-                playerEntity:TakeDamage(2, DamageFlag.DAMAGE_NO_PENALTIES, EntityRef(itemPickup), 0)
-                local entityList = Isaac.GetRoomEntities()
-                for _,entity in pairs(entityList) do
-                    if entity.Type == EntityType.ENTITY_EFFECT and entity.Variant == EffectVariant.SHOP_SPIKES then
-                        if entity.Position.X == itemPickup.Position.X and entity.Position.Y == itemPickup.Position.Y then
-                            Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.SHOP_SPIKES, 1, entity.Position, Vector(0,0), nil)
-                            entity:Remove()
-                        end
-                    end
-                end
-
-            elseif itemPrice == PickupPrice.PRICE_SOUL then
-                if not playerEntity:HasTrinket(tYourSoulID, false) then
-                    return nil
-                end
-                if not playerEntity:TryRemoveTrinket(tYourSoulID) then
-                    return nil
-                end
-                playerEntity:TryRemoveTrinketCostume(tYourSoulID)
-
-            elseif itemPrice > 0 then
-                local playersCoins = playerEntity:GetNumCoins()
-                if (itemPrice > playersCoins) then
-                    return nil
-                else
-                    playerEntity:AddCoins(-itemPrice)
-                end
-
-            else
-                return nil
+    if (itemPickup:IsShopItem()) then
+        -- Kill entity if it's not one of the first 4 players
+        for i=0, 3 do
+            if GetPtrHash(sourceEntity) == GetPtrHash(Isaac.GetPlayer(i)) then
+                goto payment
             end
         end
+        sourceEntity.Kill()
+        goto payed
+
+        ::payment::
+        playerEntity = sourceEntity:ToPlayer()
+        itemPrice = itemPickup.Price
+
+        -- Player pays price for the item if he can
+        if itemPrice == PickupPrice.PRICE_ONE_HEART then
+            if playerEntity:GetMaxHearts() < 2 then
+                return nil
+            end
+            playerEntity:AddMaxHearts(-2)
+
+        elseif itemPrice == PickupPrice.PRICE_TWO_HEARTS then
+            local maxHearts = playerEntity:GetMaxHearts()
+            if maxHearts < 2 then
+                return nil
+            elseif maxHearts >= 4 then
+                maxHearts = 4
+            end
+            playerEntity:AddMaxHearts(-maxHearts)
+
+        elseif itemPrice == PickupPrice.PRICE_THREE_SOULHEARTS then
+            local maxHeartsSoul = playerEntity:GetSoulHearts()
+            if maxHeartsSoul < 1 then
+                return nil
+            elseif maxHeartsSoul >= 6 then
+                maxHeartsSoul = 6
+            end
+            playerEntity:AddSoulHearts(-maxHeartsSoul)
+
+        elseif itemPrice == PickupPrice.PRICE_ONE_HEART_AND_TWO_SOULHEARTS then
+            local maxHearts = playerEntity:GetMaxHearts()
+            local maxHeartsSoul = playerEntity:GetSoulHearts()
+            if maxHearts < 2 then
+                return nil
+            elseif maxHeartsSoul > 4 then
+                maxHeartsSoul = 4
+            end
+            playerEntity:AddMaxHearts(-maxHearts)
+            playerEntity:AddSoulHearts(-maxHeartsSoul)
+
+        elseif itemPrice == PickupPrice.PRICE_SPIKES then
+            playerEntity:TakeDamage(2, DamageFlag.DAMAGE_NO_PENALTIES, EntityRef(itemPickup), 0)
+            local entityList = Isaac.GetRoomEntities()
+            for _,entity in pairs(entityList) do
+                if entity.Type == EntityType.ENTITY_EFFECT and entity.Variant == EffectVariant.SHOP_SPIKES then
+                    if entity.Position.X == itemPickup.Position.X and entity.Position.Y == itemPickup.Position.Y then
+                        Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.SHOP_SPIKES, 1, entity.Position, Vector(0,0), nil)
+                        entity:Remove()
+                    end
+                end
+            end
+
+        elseif itemPrice == PickupPrice.PRICE_SOUL then
+            if not playerEntity:HasTrinket(TrinketType.TRINKET_YOUR_SOUL, false) then
+                return nil
+            end
+            if not playerEntity:TryRemoveTrinket(TrinketType.TRINKET_YOUR_SOUL) then
+                return nil
+            end
+            playerEntity:TryRemoveTrinketCostume(TrinketType.TRINKET_YOUR_SOUL)
+
+        elseif itemPrice > 0 then
+            local playersCoins = playerEntity:GetNumCoins()
+            if (itemPrice > playersCoins) then
+                return nil
+            else
+                playerEntity:AddCoins(-itemPrice)
+            end
+
+        else
+            return nil
+        end
+
+        ::payed::
         -- Make item free
         itemPickup.Price = 0
 
@@ -209,18 +213,17 @@ function BetterVoiding:payNearestItem(sourceEntity)
         end
     end
 
-    return itemPickup --return nearest item
+    return itemPickup --return payed item
 end
 
 ----------------------------------------------------------------
 -- Prepares everything for voiding NEAREST item to sourceEntity
------ @Return: Nearest item
+----- @Return: Nearest item if it could be payed
 ----------------------------------------------------------------
 function BetterVoiding:betterVoidingNearestItem(sourceEntity)
-    local item = BetterVoiding:payNearestItem(sourceEntity)
+    local item = BetterVoiding:payItem(BetterVoiding:getNearestItem(sourceEntity), sourceEntity)
 
-    managePickupIndex(item)
-    return item
+    return managePickupIndex(item)
 end
 
 -------------------------------------------------------------------------------------------------
@@ -228,54 +231,55 @@ end
 ----- @Return: Table of (Keys: remaining voidable items, Values: Distance to sourceEntity)
 -------------------------------------------------------------------------------------------------
 function BetterVoiding:betterVoidingAllItems(sourceEntity)
-    local result = {}
+    BetterVoiding:payItem(BetterVoiding:getNearestItem(sourceEntity), sourceEntity)
 
-    BetterVoiding:payNearestItem(sourceEntity)
-    manageAllPickupIndices()
-    result = TableEx.copy(collDist)
-    for item,_ in pairs(result) do
-        if item:IsShopItem() then
-            result[item] = nil
-        end
-    end
-    return TableEx.updateTable(result)
+    return manageAllPickupIndices(sourceEntity)
 end
 
 
 --        <<< Including removing item(s) and play animation >>>
 
------------------------------------------------------------------------
--- Prepares everything for voiding NEAREST item to sourceEntity
------ @Return: CollectibleType/EntitySubtye of nearest item
------------------------------------------------------------------------
+----------------------------------------------------------------------------------
+-- Voiding NEAREST item to sourceEntity
+----- @Return: CollectibleType/EntitySubtye of nearest item if it could be payed
+----------------------------------------------------------------------------------
 function BetterVoiding:betterVoidingNearestItemRA(sourceEntity)
     local item = BetterVoiding:betterVoidingNearestItem(sourceEntity)
     local collType = nil
     if item ~= nil then
         collType = item.SubType
         Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, item.Position, Vector(0,0), item) -- play animation
-        despawnItem(item)
+        item:Remove()
     end
     return collType
 end
 
--------------------------------------------------------------------------------------------------
--- Prepares everything for voiding ALL items next to sourceEntity !!!(pays only nearest item)!!!
+-------------------------------------------------------------------------------------------
+-- Voiding ALL items next to sourceEntity !!!(pays only nearest item)!!!
 ----- @Return: Table of (Values: CollectibleTypes/EntitySubtypes of all voided items)
--------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------
 function BetterVoiding:betterVoidingAllItemsRA(sourceEntity)
     local items = BetterVoiding:betterVoidingAllItems(sourceEntity)
     local collTypes = {}
     for item,_ in pairs(items) do
-        if item.Price == 0 then
-            table.insert(collTypes, item.SubType)
-            Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, item.Position, Vector(0,0), item) -- play animation
-            despawnItem(item)
-        end
+        table.insert(collTypes, item.SubType)
+        Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, item.Position, Vector(0,0), item) -- play animation
+        item:Remove()
     end
     return collTypes
 end
 
+
+----------------------------------------------------
+-- Test
+local debugText = ""
+
+function BetterVoiding:drawDebugText()
+    Isaac.RenderText(debugText, 50, 50, 255, 0, 0, 255)
+end
+
+BetterVoiding:AddCallback(ModCallbacks.MC_POST_RENDER, BetterVoiding.drawDebugText)
+----------------------------------------------------
 
 ---------------------------------------------------------------------------------------------------------
 -- ModCallbacks
